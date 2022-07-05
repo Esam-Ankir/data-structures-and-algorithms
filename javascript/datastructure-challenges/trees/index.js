d361538bc8b0fafea6b4021c5c763a8439567df6
Esam-Ankir <eankir@hotmail.com>
2022-07-05 23:59:06 +0300

    findMax

--- javascript/datastructure-challenges/trees/index.js
+++ javascript/datastructure-challenges/trees/index.js
@@ -3,41 +3,8 @@ const BinaryTree = require('./binary-tree');
 const BinarySearchTree = require('./binarySearchTree');
 const Node = require('./node');
 
-////binary search tree///////////////////////////////////////////////////////////////
-let ten = new Node(10);
-let five = new Node(5);
-let two = new Node(2);
-let seven = new Node(7);
-let fivteen = new Node(15);
-let twelve = new Node(12);
-let seventeen = new Node(17);
-
-ten.left = five;
-ten.right = fivteen;
-five.left = two;
-five.right = seven;
-fivteen.left = twelve;
-fivteen.right = seventeen;
-
-let tree2 = null;
-
-tree2 = new BinarySearchTree(ten);
-
-// let preOrder = tree2.preOrder();
-// console.log('pre order: ', preOrder);
-tree2.add(18);
-tree2.add(4);
-console.log(tree2.contains(1));
-console.log(tree2.contains(4));
-console.log(tree2.contains(7));
-console.log(tree2.root);
-
-const newTree = new BinarySearchTree();
-newTree.add(10);
-newTree.add(11);
-newTree.add(12);
-
 ////binary tree////////////////////////////////////////////////////////////////////////
+console.log('==================binary tree==================');
 let tree = null;
 
 let one = new Node(1);
@@ -60,6 +27,7 @@ three.left = four;
 three.right = five5;
 
 tree = new BinaryTree(one);
+console.log("max=",tree.findMax());
 
 let preOrder = tree.preOrder();
 console.log('pre order: ', preOrder);
@@ -73,4 +41,70 @@ console.log('in order: ', inOrder);
 let postOrder = tree.postOrder();
 console.log('post order: ', postOrder);
 //pre order > 8 , 9 , 7 , 6 , 2 , 4 , 5 , 3 , 1
+///////////////////////////////
+// let tree = null;
+
+// let one = new Node(1);
+// let two2 = new Node(2);
+// let three = new Node(3);
+// let four = new Node(4);
+// let five5 = new Node(5);
+// let six = new Node(6);
+// let seven7 = new Node(7);
+// let eight = new Node(8);
+// let nine = new Node(9);
+
+// one.left = seven7;
+// one.right = five5;
+// seven7.left = eight;
+// seven7.right = two2;
+// five5.left = four;
+// five5.right = six;
+// four.right = three;
+// four.left = nine;
+
+
+// tree = new BinaryTree(one);
+// console.log("max=",tree.findMax());
+
+
+
+
+////binary search tree///////////////////////////////////////////////////////////////
+console.log('==================binary search tree==================');
+
+let ten = new Node(10);
+let five = new Node(5);
+let two = new Node(2);
+let seven = new Node(7);
+let fivteen = new Node(15);
+let twelve = new Node(12);
+let seventeen = new Node(17);
+
+ten.left = five;
+ten.right = fivteen;
+five.left = two;
+five.right = seven;
+fivteen.left = twelve;
+fivteen.right = seventeen;
+
+let tree2 = null;
+
+tree2 = new BinarySearchTree(ten);
+
+tree2.add(18);
+tree2.add(4);
+console.log(tree2.contains(1));
+console.log(tree2.contains(4));
+console.log(tree2.contains(7));
+console.log('pre order: ', tree2.preOrder());
+
+console.log("max=",tree2.findMax());
+
+const newTree = new BinarySearchTree();
+newTree.add(10);
+newTree.add(11);
+newTree.add(12);
+
+
 
